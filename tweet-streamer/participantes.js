1cd039f1150afb2351fbe668cf1ff0e373de2ce4
cdbm <cdbm@cin.ufpe.br>
2021-02-20 22:14:32 -0300

    Streaming de tweets de participantes

---
+++ tweet-streamer/participantes.js
@@ -0,0 +1,85 @@
+let BBB = [
+    {
+        nome: 'BBB',
+        tweets: 0,
+        queries: ['#bbb','#bbb21']
+    },
+    {
+        nome: 'Gilberto',
+        tweets: 0,
+        queries: ['gilberto','gil']
+    },
+    {
+        nome: 'Sarah',
+        tweets: 0,
+        queries: ['sarah']
+    },
+    {
+        nome: 'Juliette',
+        tweets: 0,
+        queries: ['juliete']
+    },
+    {
+        nome: 'Arthur',
+        tweets: 0,
+        queries: ['arthur']
+    },
+    {
+        nome: 'Projota',
+        tweets: 0,
+        queries: ['projota','probosta']
+    },
+    {
+        nome: 'Karol Conka',
+        tweets: 0,
+        queries: ['karol','karol conka', 'jaque', 'jaque patomba']
+    },
+    {
+        nome: 'João',
+        tweets: 0,
+        queries: ['joao', 'joão']
+    },
+    {
+        nome: 'Lumena',
+        tweets: 0,
+        queries: ['lumena', 'lumenas']
+    },
+    {
+        nome: 'Thaís',
+        tweets: 0,
+        queries: ['thais', 'thaís']
+    },
+    {
+        nome: 'Carla',
+        tweets: 0,
+        queries: ['carla']
+    },
+
+    {
+        nome: 'Camilla de Lucas',
+        tweets: 0,
+        queries: ['camilla', 'camila']
+    },
+    {
+        nome: 'Pocah',
+        tweets: 0,
+        queries: ['pocah']
+    },
+    {
+        nome: 'Rodolffo',
+        tweets: 0,
+        queries: ['rodolffo', 'rodolfo']
+    },
+    {
+        nome: 'Viih Tube',
+        tweets: 0,
+        queries: ['viih', 'tube', 'vih']
+    },
+    {
+        nome: 'Fiuk',
+        tweets: 0,
+        queries: ['fiuk', 'zumbi']
+    }
+]
+
+module.exports = BBB
---
+++ tweet-streamer/streamer.js
@@ -0,0 +1,51 @@
+const Twit = require('twit');
+const keys = require('./keys');
+let BBB = require('./participantes');
+
+var T = new Twit(keys);
+
+
+let stream = T.stream('statuses/filter', { track: ['#bbb','#bbb21'] });
+
+
+stream.on('tweet', function (tweet) {
+    let TweetObject = getTweetObject(tweet);
+    let text = TweetObject.text.toLowerCase();
+    BBB.forEach(elem => {
+        elem.queries.forEach(query => {
+            if(text.includes(query)){
+                elem.tweets++;
+                return;
+            }
+        })
+    })
+});
+
+stream.on('error', function(err){
+    console.log(err)
+})
+
+
+
+function getTweetObject(tweet) {
+    let tweetText = (tweet.extended_tweet) ? tweet.extended_tweet.full_text : tweet.text;
+
+    // check for retweets
+    if (tweet.text.includes('RT @') && tweet.retweeted_status) {
+        tweetText = (tweet.retweeted_status.extended_tweet) ? tweet.retweeted_status.extended_tweet.full_text : tweet.retweeted_status.text;
+    }
+
+    let TweetObject = {
+        text: tweetText,
+        user: tweet.user.name,
+        location: (tweet.user.location !== null) ? tweet.user.location : '',
+        followers: tweet.user.followers_count,
+        userImage: tweet.user.profile_image_url,
+        timestamp: tweet.timestamp_ms,
+    };
+
+    return TweetObject;
+}
+
+
+let isStreamStopped = false;
